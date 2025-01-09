931185f42a07ef64d4f941da257aef2e6229a925
Edmar Campos <edmarcamposc97@gmail.com>
2025-01-09 01:01:40 -0500

    Search feature implemented

--- evaluate-service/context-assisted-generation/embedding-record.ts
+++ evaluate-service/context-assisted-generation/embedding-record.ts
@@ -1,46 +1,84 @@
-const { transform } = require("camaro");
+import { GoogleCloudStorageDocstore } from "@langchain/community/stores/doc/gcs";
 import { MatchingEngine } from "@langchain/community/vectorstores/googlevertexai";
-import fs from "fs";
-
-interface DocumentMetadata {
-  id: string;
-  keywords: string[];
-  journal: string;
-  title: string;
-  abstract: string;
-}
+import { SyntheticEmbeddings } from "@langchain/core/utils/testing";
+import { parseXMLtoDocument } from "./parse-xml";
+import { Readable, Writable } from "stream";
+import { pipeline } from "stream/promises";
+import { api } from "encore.dev/api";
 
 interface Record {
-  id: number;
-  pageContents: string;
+  id: string;
+  pageContent: string;
   metadata: {
     source: string; //to reference
     topic: string; //journal-title, to speed up retrieval
     keywords: string;
     title: string;
-    abstract: string;
-    embedded?: any; //tittle+embedded
   };
 }
 
-const template = {
-  keywords: `/collection/document/passage[1]/infon[@key='kwd']`,
-  journal: `/collection/document/passage[1]/infon[@key='journal-title']`,
-  title: `collection/document/passage[1]/text`,
-  abstract: `collection/document/passage[2]/text`,
-  id: "collection/document/id",
-};
+const API_ENDPOINT = "283623269.us-west1-669513239525.vdb.vertexai.goog";
+const INDEX_ENDPOINT =
+  "projects/669513239525/locations/us-west1/indexEndpoints/3269358242773336064";
+const DEPLOYED_INDEX_ID = "hkw_index_1736395795542";
 
-async function parseXMLtoDocument(
-  filePath: string,
-): Promise<DocumentMetadata | null> {
-  try {
-    const xmlContent = fs.readFileSync(filePath, "utf-8");
-    return (await transform(xmlContent, template)) as DocumentMetadata;
-  } catch (error) {
-    console.error("Error parsing XML:", error);
-    return null;
+const starterEmbeddings = new SyntheticEmbeddings({ vectorSize: 768 });
+const gstorage = new GoogleCloudStorageDocstore({
+  bucket: process.env.GCLOUD_STORAGE_BUCKET!,
+});
+const vectorIndex = new MatchingEngine(starterEmbeddings, {
+  index: process.env.VERTEXAI_INDEX_ID!,
+  indexEndpoint: process.env.VERTEXAI_INDEX_ENDPOINT_ID!,
+  apiEndpoint: API_ENDPOINT,
+  endpoint: INDEX_ENDPOINT,
+  deployedIndexId: DEPLOYED_INDEX_ID,
+  apiVersion: "v1",
+  docstore: gstorage,
+});
+//console.log("PUBLIC ENDPOINT", await vectorIndex.determinePublicAPIEndpoint());
+const dir = process.env.DIR_PATH;
+const start = parseInt(process.env.START || "0");
+const end = parseInt(process.env.END || "0");
+const size = 1000;
+
+async function getBatch(dir: string | undefined, start: number, n: number) {
+  if (!dir) return null;
+  const batch: Record[] = [];
+  for (let i = start; i <= start + n; i++) {
+    const filePath = `${dir}${3000000 + i}.xml`; // Update this to the actual file path logic // Update this to the actual file path logic
+    const fileData = await parseXMLtoDocument(filePath);
+    if (!fileData) continue;
+    const record: Record = {
+      id: fileData.id,
+      pageContent: fileData.title.concat(fileData.abstract),
+      metadata: {
+        source: fileData.id,
+        keywords: fileData.keywords,
+        topic: fileData.journal,
+        title: fileData.journal,
+      },
+    };
+    console.log("batch:", start);
+    batch.push(record);
   }
+  return batch;
 }
 
-const engine = new MatchingEngine();
+const streamBatches = async () => {
+  try {
+    for (let i = start; i <= end + 1; i = i + size) {
+      const batch = await getBatch(dir, i, i + size);
+      if (!batch) continue;
+      await vectorIndex.addDocuments(batch);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const streamRecords = api(
+  { expose: true, method: "GET", path: "/records" },
+  async () => {
+    await streamBatches();
+  },
+);
--- todo.ts
+++ todo.ts
@@ -1,9 +1,8 @@
 /**
  * ROADMAP
- * TODO: Prepare embedded data /evaluate/embed-records/1
  * TODO: Create an index from data /evaluate/set-index/1
  * TODO: Dashboard /front-end/1
- * TODO: Search feature /search/2
+ * DONE: Search feature /search/2
  * TODO: User-evaluations database /store/db/2
  * TODO: Database record flow (towards search and from
  *      evaluate and update-user-evaluations) /store/operations/2
